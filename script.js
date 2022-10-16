e8268d125a4a2c565ee6db49a57bd58834291ac8
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-10-16 13:50:48 +0400

    fix commit

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var button = document.querySelector('.add_more');
+var list = document.querySelector('.time');
 var listItem = document.querySelector('.time').querySelectorAll('li');
+var schedule = document.querySelector('.card__schedule');
 
-
-listItem.forEach((li) => {
-    button.addEventListener('click', () =>{
-        li.classList.toggle('')
+if(listItem.length > 4){
+    var button = schedule.appendChild(document.createElement('button'));
+    button.classList.add('add_more');
+    button.innerHTML = 'Ещё...';
+    listItem.forEach((li) => {
+        button.addEventListener('click', () => {
+            li.classList.add('open');
+            button.style.display = 'none';
+        })
     })
-}) 
+}
+
+
 
 
 
