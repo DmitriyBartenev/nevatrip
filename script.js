334e92745bbe5b0b4934ad27af773b9156c3027b
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-10-16 20:37:30 +0400

    fix

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var list = document.querySelector('.time');
 var listItem = document.querySelector('.time').querySelectorAll('li');
-var schedule = document.querySelector('.card__schedule');
 
-if(listItem.length > 4){
-    var button = schedule.appendChild(document.createElement('button'));
+
+var time = document.querySelectorAll('.time');
+var timeItem = document.querySelectorAll('.time__item');
+
+time.forEach((ul) => {
+    var button = ul.appendChild(document.createElement('button'));
     button.classList.add('add_more');
     button.innerHTML = 'Ещё...';
-    listItem.forEach((li) => {
+    timeItem.forEach((li) => {
         button.addEventListener('click', () => {
             li.classList.add('open');
-            button.style.display = 'none';
+            var buttons = document.querySelectorAll('.add_more')
+            buttons.forEach(button => button.remove())
         })
     })
-}
+})
 
 
 
